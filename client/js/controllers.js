4054c427300c243dda6953f336ffc68989daf852
elvece <lvcifferello@gmail.com>
2015-10-08 10:43:28 -0600

    data populates for get single post page

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,7 +1,5 @@
 app.controller('mainController', function($scope, myFactory, $http, $location, $routeParams, $filter){
 
-  // $scope.posts = {};
-
   //helper function
   getPosts = function(url){
     myFactory.get(url)
@@ -13,27 +11,6 @@ app.controller('mainController', function($scope, myFactory, $http, $location, $
   //get all posts
   $scope.getAllPosts = getPosts('/api/posts');
 
-  $scope.getPost = function(id){
-    $scope.thisFuckingThing = $filter('filter')($scope.posts, {_id: id});
-    // var test = $scope.posts.filter(function(obj){
-    //   return $routeParams.firstName === obj.firstName;
-    // });
-console.log('does this exist? ', $scope.thisFuckingThing)
-console.log(test);
-console.log('should be obj ', test[0]);
-console.log('should be name: ', test[0].firstName);
-
-
-  };
-
-//function to
-
-  $scope.getSingleBlogPost = function(){
-    myFactory.get('api/post/' + id)
-      .then(function(res){
-
-      });
-  };
 
   $scope.addBlogPost = function(){
     myFactory.post('/api/posts', $scope.newPost)
@@ -49,7 +26,20 @@ console.log('should be name: ', test[0].firstName);
         $scope.getAllPosts;
       });
   };
+});
 
+app.controller('postController', function($scope, myFactory, $http, $location, $routeParams, $filter){
+
+  $scope.thing = $routeParams._id;
+
+  getSinglePost = function(id){
+    myFactory.get('/api/post/' + id)
+      .then(function(res){
+        $scope.post = res.data;
+      });
+  };
+
+  getSinglePost($scope.thing);
 
 });
 
