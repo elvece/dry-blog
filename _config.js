740529ff86387d2815b48a867b75ed033b857505
elvece <lvcifferello@gmail.com>
2015-10-29 11:53:53 -0600

    config out of gitignore

---
+++ _config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'my-precious',
+  MONGOLAB_URI: process.env.MONGOLAB_URI || 'mongodb://localhost/dry-users' && 'mongodb://localhost/dry-posts',
+  SALT_WORK_FACTOR: 10
+};
