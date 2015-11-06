b021a9600f3d0950439d1ef66c33332e4cacdfc1
ZoeAde <zoe.a.adelman@gmail.com>
2015-11-06 08:11:26 -0700

    adding unique titles to pages

--- client/js/app.js
+++ client/js/app.js
@@ -2,3 +2,9 @@ var app = angular.module('dryApp', ['ngRoute', 'angularMoment', 'duScroll', 'sat
 
 app.value('duScrollDuration', 1400);
 // app.value('duScrollOffset', 100);
+
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
--- client/js/routes.js
+++ client/js/routes.js
@@ -3,38 +3,47 @@
 app.config(function($routeProvider, $locationProvider, $authProvider){
   $routeProvider
     .when('/', {
+      title: 'Dry Blog',
       templateUrl: 'views/home.html',
-      controller: 'mainController',
+      controller: 'mainController'
     })
     .when('/login', {
+      title: 'Login',
       templateUrl: 'views/login.html',
-      controller: 'loginController',
+      controller: 'loginController'
     })
     .when('/signup', {
+      title: 'Signup',
       templateUrl: 'views/signup.html',
-      controller: 'signupController',
+      controller: 'signupController'
     })
     .when('/about', {
+      title: 'About Dry Blog',
       templateUrl: 'views/about.html',
-      controller: 'mainController',
+      controller: 'mainController'
     })
     .when('/creators', {
+      title: 'Dry Blog Creators',
       templateUrl: 'views/creators.html',
-      controller: 'mainController',
+      controller: 'mainController'
     })
     .when('/add', {
+      title: 'Add Interview',
       templateUrl: 'views/add.html',
       controller: 'mainController'
     })
     .when('/twitter', {
+      title: 'Dry Blog Twitter Feed',
       templateUrl: 'views/twitter.html',
       controller: 'mainController'
     })
     .when('/resources', {
+      title: 'Community Resources',
       templateUrl: 'views/resources.html',
       controller: 'mainController'
     })
     .when('/post/:lastName/:firstName', {
+      title: 'Interview',
       templateUrl: 'views/individual.html',
       controller: 'postController'
     })
