562c2ac26603df78c613569ab8c8aa7f6b4c2e4c
elvece <lvcifferello@gmail.com>
2015-11-13 12:27:14 -0700

    fixed route title error

--- client/js/app.js
+++ client/js/app.js
@@ -6,6 +6,6 @@ app.value('duScrollDuration', 1400);
 //creating different title name on each page in browser tab
 app.run(['$rootScope', function($rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-        $rootScope.title = current.$route.title;
+        $rootScope.title = current.$$route.title;
     });
 }]);
