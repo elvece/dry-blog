24e80ea432e8343160dcfeae8a29fb4bf6e249f7
elvece <lvcifferello@gmail.com>
2015-11-12 22:41:30 -0700

    switched back db names

--- _config.js
+++ _config.js
@@ -1,8 +1,8 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'my-precious',
-  MONGOLAB_URI: process.env.MONGOLAB_URI || 'mongodb://localhost/dryUsers' && 'mongodb://localhost/dryPosts',
+  MONGOLAB_URI: process.env.MONGOLAB_URI || 'mongodb://localhost/dry-users' && 'mongodb://localhost/dry-posts',
   SALT_WORK_FACTOR: 10
 };
 
 
-//mongodb://localhost/dry-users' && 'mongodb://localhost/dry-posts
+//mongodb://localhost/dryUsers' && 'mongodb://localhost/dryPosts
